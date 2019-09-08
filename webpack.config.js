a216aeb8a2c018377f545f97aac3ae8b0b1871cc
pecology <dummy>
2019-09-08 17:43:14 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+      path: __dirname,
+      filename: './dist/bundle.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader'
+        }
+      ]
+    },
+    devServer: {
+        watchContentBase: true,
+        open: true
+    }
+  }
